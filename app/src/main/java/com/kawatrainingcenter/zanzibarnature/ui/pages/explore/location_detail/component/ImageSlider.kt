3681fce13c1f91ac0851c2f6e8f1c8a8b4e2f79a
JulieMeij <54932056+JulieMeij@users.noreply.github.com>
2023-12-20 15:09:43 +0100

    filename changed

--- app/src/main/java/com/kawatrainingcenter/zanzibarnature/ui/pages/explore/location_detail/component/ImageSlider.kt
+++ app/src/main/java/com/kawatrainingcenter/zanzibarnature/ui/pages/explore/location_detail/component/ImageSlider.kt
@@ -2,26 +2,17 @@ package com.kawatrainingcenter.zanzibarnature.ui.pages.explore.location_detail.c
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
 
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
@@ -48,7 +39,7 @@ fun ImageSlider(
 
     Box(
         modifier = Modifier
-            .height(350.dp)
+            .height(310.dp)
             .fillMaxWidth()
     ) {
 
@@ -77,12 +68,13 @@ fun ImageSlider(
         LazyRow(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 250.dp),
+                .padding(top = 245.dp),
             horizontalArrangement = Arrangement.Center
         ) {
             items(images.count()) { index ->
                 Text(
                     text = ".",
+                    letterSpacing = 6.sp,
                     style = TextStyle(
                         fontSize = 50.sp,
                         fontWeight = FontWeight(400),
